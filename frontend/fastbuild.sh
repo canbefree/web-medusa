de1a806ff47e3b168a3e1cc1588466ba17122f5f
123 <123123>
2022-08-20 08:59:13 +0000

    sav

--- frontend/fastbuild.sh
+++ frontend/fastbuild.sh
@@ -1,1 +1,14 @@
- npm config set registry https://registry.npm.taobao.org & npm install
+npm config set registry https://registry.npm.taobao.org & npm install
+
+
+if [ -f /workspace/.init_dev_env.sh ]; then 
+    source /workspace/.init_dev_env.sh
+fi
+
+if [ ! -z $GIT_USERNAME ];then
+    git config --global user.name $GIT_USERNAME
+fi
+
+if [ ! -z $GIT_EMAIL ] ;then
+    git config --global user.email $GIT_EMAIL
+fi
