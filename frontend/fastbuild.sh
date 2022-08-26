aff9dbec8b23da1d593cbbb35c5b6aed6c5d0fc5
canbefree <452198757@qq.com>
2022-08-26 03:06:37 +0000

    fastbuild

--- frontend/fastbuild.sh
+++ frontend/fastbuild.sh
@@ -1,5 +1,14 @@
-npm config set registry https://registry.npm.taobao.org & npm install
+#! /bin/bash 
+set -e 
+# 淘宝
+# npm config set registry https://registry.npmmirror.com & npm install
+# 腾讯
+npm config set registry http://mirrors.cloud.tencent.com/npm/ & npm install
+# 华为
+# npm config set registry https://mirrors.huaweicloud.com/repository/npm/ & npm install 
 
+# 验证
+npm config get registry
 
 if [ -f /workspace/.init_dev_env.sh ]; then 
     source /workspace/.init_dev_env.sh
